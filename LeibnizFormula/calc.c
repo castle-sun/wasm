f9bbf636284a394d56834055f75fcc4eb75a1811
castle-sun <castleinthesky1@hotmail.com>
2022-10-08 12:42:57 +0900

    commit

---
+++ LeibnizFormula/calc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+int leibniz(int n)
+{
+    double s = 0.0;
+    for (int i = 0; i<=n; i++) {
+        s += pow(-1.0, i) / (2.0 * i + 1.0);
+    }
+    return s * 4;
+}
+
+int main(int argc, char *argv[])
+{
+    clock_t begin, end;
+    begin = clock();
+
+    int n = 100000000;
+    int result = leibniz(n);
+
+    end = clock();
+    printf("結果[%d],処理時間[%lf][S]\n",result,difftime(end, begin) / CLOCKS_PER_SEC);
+    
+    return 0;
+}
